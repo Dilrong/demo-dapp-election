1f3dd2229650998869548e4f79727125d09e04a3
dilrong <dilrong@dilrong.com>
2021-09-10 12:58:34 +0900

    feat: frontend design

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,21 +2,27 @@
 pragma solidity ^0.8.4;
 
 contract Election {
-    // Modal a Candidate
+    // Model a Candidate
     struct Candidate {
         uint id;
         string name;
         uint voteCount;
     }
 
-    // Store Candidate
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
+    // Store Candidates
     // Fetch Candidate
     mapping(uint => Candidate) public candidates;
-
-    // Store Candidate Count
+    // Store Candidates Count
     uint public candidatesCount;
 
-    function setElection () public {
+    // voted event
+    event votedEvent (
+        uint indexed _candidateId
+    );
+
+    constructor () {
         addCandidate("Candidate 1");
         addCandidate("Candidate 2");
     }
@@ -24,5 +30,22 @@ contract Election {
     function addCandidate (string memory _name) private {
         candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote (uint _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount ++;
+
+        // trigger voted event
+        emit votedEvent(_candidateId);
     }
 }
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,53 @@
 import React from "react";
+import { Layout, Typography, Button, List, Select, Space } from "antd";
+import { GithubOutlined } from "@ant-design/icons";
+import "./App.css";
+
+const { Header, Footer, Content } = Layout;
+const { Title } = Typography;
+const { Option } = Select;
+
+function handleChange(value: any) {
+  console.log(`selected ${value}`);
+}
 
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <Layout className="layout">
+        <Header style={{ display: "flex", alignItems: "center" }}>
+          <Title level={2} style={{ color: "white", margin: 0 }}>
+            Election Demo Dapp
+          </Title>
+        </Header>
+        <Content style={{ padding: "0 50px" }}>
+          <div className="site-layout-content">
+            <List
+              header={<Title level={4}>Election Results</Title>}
+              bordered
+              // renderItem={<List.Item></List.Item>}
+            />
+            <Space style={{ paddingTop: 24 }}>
+              <Select defaultValue="lucy" onChange={handleChange}>
+                <Option value="lucy">lucy</Option>
+              </Select>
+              <Button type="primary">Vote</Button>
+            </Space>
+          </div>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          <Button
+            shape="circle"
+            icon={<GithubOutlined />}
+            onClick={() => {
+              document.location.href =
+                "https://github.com/Dilrong/demo-dapp-election";
+            }}
+          />
+        </Footer>
+      </Layout>
+    </>
+  );
 }
 
 export default App;
